ce563b38cce0f5b0237cc6a05299da157d3bd009
Michel Camatta <camattamichel@gmail.com>
2021-10-02 13:33:19 -0300

    Início da modelagem

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,16 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    recupera() {
+        const exp = /-/g;
+        const data = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        const negociacao = new Negociacao(data, quantidade, valor);
+        console.log(negociacao);
+    }
+}
