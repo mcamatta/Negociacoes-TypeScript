b5ad01a56c59ccc11ad100bf4a6f8be23a6d0701
Michel Camatta <camattamichel@gmail.com>
2021-10-02 19:38:49 -0300

    Ajustes na leitura do código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,24 +1,17 @@
 
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
-
-    constructor(data: Date, quantidade: number, valor: number){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private _data: Date, 
+        private readonly _quantidade: number, /* Apenas leitura */
+        private readonly _valor: number
+    ){}
 
     get data(): Date{
+        const data = new Date(this._data.getTime()); // programação defensiva
         return this._data;
     }
 
-    get quantidade(): number{
-        return this._quantidade;
-    }
-
-    get valor(): number{
-        return this._valor;
+    get volume(): number{
+        return this._quantidade * this._valor;
     }
 }
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes{
-    private negociacoes: Array<Negociacao> = []; //generic :)
+    private negociacoes: Array<Negociacao> = []; //generic :) / Negociacao[]
 
     adiciona(negociacao: Negociacao): void{
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao>{ //retorna apenas para a leitura
+    lista(): ReadonlyArray<Negociacao>{ //retorna apenas para a leitura / readonly Negociacao[]
         return this.negociacoes;
     }
 }
