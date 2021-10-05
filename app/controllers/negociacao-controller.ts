cf9547e8ca18e52536e3910c0e77018230274f68
Michel Camatta <camattamichel@gmail.com>
2021-10-05 10:10:39 -0300

    criação da view

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,23 +1,27 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController{
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+
     private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView("#negociacoesView");
 
     constructor(){
         this.inputData = document.querySelector("#data");
         this.inputQuantidade = document.querySelector("#quantidade");
         this.inputValor = document.querySelector("#valor");
+        this.negociacoesView.update();
     }
 
     recupera(): void{
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         const negociacoes = this.negociacoes.lista();
-        console.log(negociacoes);
+
         this.limpaFormulario();
     }
 
